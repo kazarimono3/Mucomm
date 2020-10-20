666db4cc8cf1b3e0ba9818815375d0b8e4dd5591
kazarimono3 <t.kaneda@KANEDA-PCnoMacBook-Pro.local>
2020-10-20 15:10:53 +0900

    コントローラーの追加

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+
+  before_action :authenticate_user!
+
+	def new
+    @post = Post.new
+    @post.audios.build
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.audios.present?
+      @post.save
+      redirect_to root_path
+      flash[:notice] = "投稿が保存されました"
+    else
+      redirect_to root_path
+      flash[:alert] = "投稿に失敗しました"
+    end
+  end
+
+  def index
+    @posts = Post.limit(10).includes(:audios, :user).order('created_at DESC')
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:caption, audios_attributes: [:file]).merge(user_id: current_user.id)
+  end
+end
