89bd7911d729e486717aadaaed36cef746a559a5
kazarimono3 <t.kaneda@KANEDA-PCnoMacBook-Pro.local>
2020-10-20 15:27:05 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_150034) do
+ActiveRecord::Schema.define(version: 2020_10_19_074723) do
+
+  create_table "audios", force: :cascade do |t|
+    t.string "audio", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_audios_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "caption"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_150034) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
