094cd9c3401b10e39c296f0c8116fb83354ddc9d
kazarimono3 <t.kaneda@KANEDA-PCnoMacBook-Pro.local>
2020-10-20 15:25:53 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ 	devise_for :users,
+  controllers: { registrations: 'registrations' }
+
  	root 'pages#home'
- end
+ 	resources :users, only: %i(show)
+
+ 	resources :posts, only: %i(new create index) do
+    resources :audios, only: %i(create)
+  end
+end
