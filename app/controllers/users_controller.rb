f3422c74a32371ad441abb40022ab229835b303a
kazarimono3 <t.kaneda@KANEDA-PCnoMacBook-Pro.local>
2020-10-29 00:44:49 +0900

    ユーザーとポストの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def user_params
+  	params.require(:user).permit(:name, :email, :image)
+	end
+
+	def avater_update
+		@user = User.find_by(id: params[:id])
+		@user.image = params[:image]
+		@user.save!
+		redirect_to user_path
+      flash[:notice] = "投稿が保存されました"
+	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,12 @@ class Post < ApplicationRecord
 
 	belongs_to :user
 	has_many :audios, dependent: :destroy
-
+	has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+	has_many :comments, dependent: :destroy
+	
 	accepts_nested_attributes_for :audios
+
+	def liked_by(user)
+    Like.find_by(user_id: user.id, post_id: id)
+  end
 end
