a929f940ee4fc88746a885c825393b823c0e1b87
kazarimono3 <t.kaneda@KANEDA-PCnoMacBook-Pro.local>
2020-10-29 00:46:53 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,19 @@ Rails.application.routes.draw do
  	devise_for :users,
   controllers: { registrations: 'registrations' }
 
- 	root 'pages#home'
- 	resources :users, only: %i(show)
+ 	root 'posts#index'
 
- 	resources :posts, only: %i(new create index) do
+ 	get '/users/:id', to: 'users#show', as: 'user'
+ 	
+ 	resources :users do
+ 		member do
+    		put :avater_update
+  		end
+ 	end
+
+	resources :posts, only: %i(index new create show destroy) do
     resources :audios, only: %i(create)
-  end
+		resources :likes, only: %i(create destroy)
+		resources :comments, only: %i(create destroy)
+  	end
 end
